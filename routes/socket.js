08fbc9183ff2b2d223596da9aeba6a8450009919
Kumpon J Sotsukpiam <tetravaal.system@gmail.com>
2019-10-26 00:13:08 +0700

    update request socketIO

--- routes/socket.js
+++ routes/socket.js
@@ -9,30 +9,28 @@ module.exports = io => {
     io.on('connection', socket => {
         console.log('Connected socketIO');
         socket.on('sendphoto', data => {
-            console.log(data);
-            
-            // const { Base64, name, type } = data
-            // pathName = `./dataSet/${name}`
-            // decodeType = type.split(";").pop()
-            // decodeBase64Image(Base64,decodeType, decode => {
-            //     // here code to send base64 to server
-            //     options = {
-            //         method:req.method,
-            //         uri:`${url}/predict`,
-            //         form:{
-            //             model_id:id,
-            //             buffer:decode
-            //         }
-            //     }
-            //     request(options).then(v => {
-            //         res.status(200).json(v)
-            //     }).catch(err => {
-            //         console.error(err)
-            //         res.status(400).json(err)
-            //     })
-            //     // end code
-            //     fs.writeFileSync(pathName, decode);
-            // })
+            const { Base64, name, type, classId, authId } = data
+            pathName = `./dataSet/${name}`
+            decodeType = type.split(";").pop()
+            decodeBase64Image(Base64,decodeType, decode => {
+                // here code to send base64 to server
+                // options = {
+                //     method:req.method,
+                //     uri:`${url}/predict`,
+                //     form:{
+                //         model_id:id,
+                //         buffer:decode
+                //     }
+                // }
+                // request(options).then(v => {
+                //     res.status(200).json(v)
+                // }).catch(err => {
+                //     console.error(err)
+                //     res.status(400).json(err)
+                // })
+                // end code
+                // fs.writeFileSync(pathName, decode);
+            })
         })
     })
     setInterval(() => {
