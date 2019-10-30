7b4d05a131287a92ee0625898890cf1270c7f7d5
Kumpon J Sotsukpiam <tetravaal.system@gmail.com>
2019-10-30 23:06:07 +0700

    update delete semester

--- routes/semester.js
+++ routes/semester.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose')
 const Semester = mongoose.model('semester')
+const Course = mongoose.model('course')
+const Class = mongoose.model('class')
+const CheckIn = mongoose.model('checkIn')
 const semesterValidator = require('../modules/semesterValidator')
 
 router.post('/', (req, res, next) => {
@@ -71,8 +74,34 @@ router.get('/', (req, res, next) => { // get All
 
 router.delete('/:_id', (req, res, next) => {
     const { _id } = req.params
-    Semester.deleteOne({ _id: _id }).then(cour => {
-        return res.status(200).json(cour)
+    Semester.deleteOne({ _id: _id }).then(semester => {
+        Course.find({semesterId:_id}).then(cour => {
+            if(!cour)
+                return res.status(200).json(semester)
+            else
+                Course.deleteMany({semesterId:_id}).then(courOk => {
+                    const couseList = []
+                    cour.map(v => couseList.push(v._id))
+                    Class.find({courseId:couseList}).then(clas => {
+                        if(!clas)
+                            return res.status(200).json(clas)
+                        else 
+                        Class.deleteMany({courseId:couseList}).then(classOk => {
+                            const classList = []
+                            clas.map(v => classList.push(v._id))
+                            CheckIn.find({classId:classList}).then(check => {
+                                if(!check)
+                                    return res.status(200).json(check)
+                                else
+                                CheckIn.deleteMany({classId:classList}).then(checkOk => {
+                                    res.status(200).json(checkOk)
+                                })
+                            })
+                        })
+                    })
+                })
+        })
+        //return res.status(200).json(cour)
     }).catch(err => {
         console.error(err)
     })
