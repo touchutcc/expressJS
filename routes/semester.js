0a5797c8c93f44a4d4efc9b5d19619b2929cf7be
moochamp <chanathip-n@hotmail.com>
2019-10-30 10:25:51 +0700

    kumpon update semester

--- routes/semester.js
+++ routes/semester.js
@@ -7,30 +7,44 @@ const semesterValidator = require('../modules/semesterValidator')
 router.post('/', (req, res, next) => {
     // standard info
     const { name, startDate, endDate } = req.body
+    const newSemester = new Semester({
+        userId: req.uid,
+        name: name,
+        startDate: startDate,//startDate,
+        endDate: endDate //endDate
+    })
+    newSemester.save().then(Semest => {
+        res.json({
+            ok: true,
+            data: Semest
+        })
+    }).catch(err => {
+        console.error(err)
+    })
     //const { errors, isValid } = semesterValidator(req.body);
     //if (!isValid) return res.status(400).json(errors)
-    Semester.findOne({ name:name }).then(Semest => {
-        if (Semest) {
-            return res.status(400).json({
-                name: 'Name already exists',
-            })
-        } else {
-            const newSemester = new Semester({
-                userId: req.uid,
-                name: name,
-                startDate: new Date().getTime(),//startDate,
-                endDate: new Date().getTime() //endDate
-            })
-            newSemester.save().then(Semest => {
-                res.json({
-                    ok: true,
-                    data: Semest
-                })
-            }).catch(err => {
-                console.error(err)
-            })
-        }
-    })
+    // Semester.findOne({ name:name }).then(Semest => {
+    //     if (Semest) {
+    //         return res.status(400).json({
+    //             name: 'Name already exists',
+    //         })
+    //     } else {
+    //         const newSemester = new Semester({
+    //             userId: req.uid,
+    //             name: name,
+    //             startDate: startDate,//startDate,
+    //             endDate: endDate //endDate
+    //         })
+    //         newSemester.save().then(Semest => {
+    //             res.json({
+    //                 ok: true,
+    //                 data: Semest
+    //             })
+    //         }).catch(err => {
+    //             console.error(err)
+    //         })
+    //     }
+    // })
 })
 
 router.put('/:_id', (req, res, next) => {
