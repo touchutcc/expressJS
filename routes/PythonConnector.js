b50d96afa9fae27589488a0bea5f177afc00ee24
Kumpon J Sotsukpiam <tetravaal.system@gmail.com>
2019-11-06 10:40:14 +0700

    backup

--- routes/PythonConnector.js
+++ routes/PythonConnector.js
@@ -3,21 +3,18 @@ const router = express.Router()
 const request = require('request-promise')
 
 const url = "http://127.0.0.1:5000"
-const pythonUrl = (path) => {
-    return `${url}/${(!path ? '': path)}`
-}
+
 router.post('/model',(req,res) => {
-    const {idList} = req.body
-    const arrayData = ["aom","bobby","game","max","new","tee"] // for test only
+    const { classId } = req.body
     options = {
         method:req.method,
         uri:`${url}/model`,
         form:{
-            data:JSON.stringify(arrayData)
+            classId:classId
         }
     }
     request(options).then(v => {
-        res.status(200).json(v)
+        res.status(200).json(JSON.parse(v))
     }).catch(err => {
         console.error(err)
         res.status(400).json(err)
