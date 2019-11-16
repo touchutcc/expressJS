0d370ab157222fd70b1ccbb8a265f4f3d73a50d2
Kumpon V Sotsukpiam <tetravaal.system@gmail.com>
2019-11-16 19:37:05 +0700

    update upload video at add Student

--- routes/student.js
+++ routes/student.js
@@ -9,15 +9,15 @@ const studentValidator = require('../modules/studentValidator')
 
 var dirServer = path.dirname(__dirname)
 var dirProject = path.dirname(dirServer)
-var PythonServer = path.join(dirProject, 'deep_server_with_facenet')
+var PythonServer = path.join(dirProject, 'vgg_deep_server')
 var dataBase = path.join(PythonServer, 'database')
 var dataUpload = path.join(dataBase, 'dataUpload')
 
-const uploadFile = async (files, stu, cb) => {
-    const uploadFile = files.file
-    const fileName = files.file.name
+const uploadFile = async (file, stu, cb) => {
+    const uploadFile = file
+    const fileName = file.name
     const fileType = fileName.split('.').pop()
-    const fileSave = `${stu._id}_${(new Date()).getTime()}.${fileType}`
+    const fileSave = `${stu._id}.${fileType}`
     const dirUploadFile = path.join(dataUpload, fileSave)
     uploadFile.mv(dirUploadFile, err => {
         cb(fileSave, err)
@@ -29,62 +29,31 @@ const deleteUploadFile = async (fileName, cb) => {
         cb(err)
     })
 }
-
-// =======================================================
-const url = "http://127.0.0.1:5000"
-const pythonUrl = (path) => {
-    return `${url}/${(!path ? '': path)}`
-}
-
-router.post('/data_set/video',(req,res,next) => {
-    options = {
-        method:req.method,
-        uri:`${url}/data_set/video`,
-        form:{
-            name:"Bobby.MOV"
-        }
-    }
-    request(options).then(v => {
-        res.status(200).json(v)
-    }).catch(err => {
-        console.error(err)
-        res.status(400).json(err)
-    })
-})
-// =======================================================
 router.post('/', (req, res, next) => {
-    const { stuId, name,major,faculty } = req.body
+    const { stuId, name, major, faculty } = req.body
     const userObjId = req.uid
-    console.log('====================================');
-    console.log(req.body);
-    console.log('====================================');
-    // Student.findOne({ stuId: stuId }).then(stu => {
-    //     if (stu)
-    //         return res.status(400).json({ stuId: 'Student ID already exists' })
-    //     else {
-    //         const newStudent = new Student({
-    //             stuId: stuId,
-    //             userId:userObjId,
-    //             name: name,
-    //             major:major,
-    //             faculty:faculty
-    //         })
-    //         newStudent.save().then(stu => {
-    //             res.json(stu)
-    //             // uploadFile(req.files, stu, (file, err) => {
-    //             //     if (err) return res.status(500).json(err)
-    //             //     deleteUploadFile(file, err => {
-    //             //         if (err) return res.status(500).json(err)
-    //             //         // face Detection
-    //             //         res.json({
-    //             //             ok: true,
-    //             //             data: stu
-    //             //         })
-    //             //     })
-    //             // })
-    //         })
-    //     }
-    // })
+    Student.findOne({ stuId: stuId }).then(stu => {
+        if (stu)
+            return res.status(400).json({ stuId: 'Student ID already exists' })
+        else {
+            const newStudent = new Student({
+                stuId: stuId,
+                userId: userObjId,
+                name: name,
+                major: major,
+                faculty: faculty
+            })
+            newStudent.save().then(stu => {
+                uploadFile(req.files.file, stu, (file, err) => {
+                    console.log('====================================');
+                    console.log(file);
+                    console.log('====================================');
+                    //if (err) return res.status(500).json(err)
+                    res.json(stu)
+                })
+            })
+        }
+    })
 })
 
 router.put('/:_id', (req, res, next) => {
@@ -107,20 +76,15 @@ router.get('/:stuId', (req, res, next) => {
     })
 })
 
-/* router.get('/', (req, res, next) => {
-    const { stuIdList } = req.body
-    Student.find({ stuId: { $in: stuIdList } }).select('_id').then(stuList => {
-        console.log('====================================');
-        console.log(stuList);
-        console.log('====================================');
-    })
-}) */
-
 router.delete('/:_id', (req, res, next) => {
     const { _id } = req.params
     Student.deleteOne({ _id: _id }).then(stu => {
-        //delete dataSet folder
-        return res.status(200).json(stu)
+        deleteUploadFile(`${_id}.mp4`, err => {
+            console.log('====================================');
+            console.log(err);
+            console.log('====================================');
+            return res.status(200).json(stu)
+        })
     }).catch(err => {
         console.error(err)
     })
